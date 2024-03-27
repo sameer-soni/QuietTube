3b9e026f442c318756b08189f00f3bf4cb549374
mohsin-mw <null.mmw@gmail.com>
2024-03-27 21:04:03 +0500

    Feat: Add Feed toggle Logic

--- content_script.js
+++ content_script.js
@@ -1,21 +1,15 @@
-//
-// const contentElement = document.querySelector("#contents");
-//
-// chrome.storage.local.get("isHidden", function(data) {
-//     console.log(data.hasOwnProperty("isHidden"))
-//     if(data.hasOwnProperty("isHidden")) {
-//         isHidden = data.isHidden;
-//         contentElement.style.display = "none";
-//     } else {
-//         isHidden = null;
-//     }
-// })
-//
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//
-//     if (request.message === "hide-feed") {
-//         isHidden = !isHidden;
-//         contentElement.style.display = contentElement.style.display === "none" ? "block" : "none";
-//         chrome.storage.local.set({ isHidden: isHidden });
-//     }
-// });
+const contentElement = document.querySelector("#contents");
+
+chrome.storage.local.get("feed", function(data) {
+    if (data.feed === true) {
+        if (contentElement) {
+            contentElement.style.display = "none";
+        }
+    }
+})
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.message === "hide-feed") {
+        contentElement.style.display = contentElement.style.display === "none" ? "block" : "none";
+    }
+});
--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const optionsSelectors = document.querySelectorAll(".options-selector");
 optionsSelectors.forEach(el => {
     const button = el;
     const toggleDot = button.querySelector("span")
-    if (button.name === "hide-notifications") {
+    if (button.name === "hide-feed") {
         chrome.storage.local.get("feed", function(data) {
             if (data.feed === true) {
                 button.classList.toggle("bg-secondary")
@@ -10,12 +10,28 @@ optionsSelectors.forEach(el => {
                 toggleDot.ariaHidden = "true"
             }
         })
+    } else if (button.name === "hide-notifications") {
+        chrome.storage.local.get("notifications", function(data) {
+            if (data.notifications === true) {
+                button.classList.toggle("bg-secondary")
+                toggleDot.classList.toggle("translate-x-5")
+                toggleDot.ariaHidden = "true"
+            }
+        })
     }
     button.addEventListener("click", function () {
-        button.classList.toggle("bg-secondary")
-        toggleDot.classList.toggle("translate-x-5");
-        toggleDot.ariaHidden = toggleDot.ariaHidden === "true" ? "false" : "true"
-        chrome.storage.local.set({ feed: toggleDot.ariaHidden === "true" });
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            button.classList.toggle("bg-secondary")
+            toggleDot.classList.toggle("translate-x-5");
+            toggleDot.ariaHidden = toggleDot.ariaHidden === "true" ? "false" : "true"
+            if (button.name === "hide-notifications") {
+                chrome.storage.local.set({ notifications: toggleDot.ariaHidden === "true" });
+            } else if (button.name === "hide-feed") {
+                chrome.storage.local.set({ feed: toggleDot.ariaHidden === "true" });
+                chrome.tabs.sendMessage(tabs[0].id, { message: button.name });
+            }
+        })
+
     })
 })
 //
